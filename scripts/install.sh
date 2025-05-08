27848fe661e645258a5f7f9a491557f0db9490ea
천명현 <cmh1448@naver.com>
2025-05-08 15:23:46 +0900

    feat: Add installation script for git-lfs and project setup

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,19 +1,22 @@
 #! /bin/bash
 
-# 가상환경 설정
-python -m venv venv
-source venv/bin/activate
-
 # git lfs 설치
+echo "Installing git-lfs..."
+curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | bash
+apt install git-lfs -y
 git lfs install
 
 # 프로젝트 다운로드
+echo "Downloading project..."
 git clone https://github.com/sgsdxzy/YuE-exllamav2
 cd YuE-exllamav2
 git clone https://huggingface.co/m-a-p/xcodec_mini_infer
+
+# requirements.txt 설치
 pip install -r requirements.txt
 
 # 모델 다운로드
+echo "Downloading models..."
 mkdir -p Doctor-shotgun
 cd Doctor-shotgun
 git clone https://huggingface.co/Doctor-Shotgun/YuE-s1-7B-anneal-en-cot-exl2
@@ -24,7 +27,3 @@ git clone https://huggingface.co/Doctor-Shotgun/YuE-s2-1B-general-exl2
 cd YuE-s2-1B-general-exl2
 git switch 8.0bpw-h8
 
-cd ../..
-
-
-# API 서버 파일 다운로드
